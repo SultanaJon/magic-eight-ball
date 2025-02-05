712c6fa8393d7db5f27c7c7e79e68a7110d97ec6
Jon Sultana <jonsultana@outlook.com>
2025-02-04 23:03:39 -0500

    Check format and format scripts. (#1)
    * Check format and format scripts.
    
    * Format files to pass format action.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,21 @@
-import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
-import './globals.css';
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
 
 const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: 'Magic Eight Ball',
+  title: "Magic Eight Ball",
   description:
-    'Magic 🎱 web application used for fortune telling and seeking advice.',
+    "Magic 🎱 web application used for fortune telling and seeking advice.",
 };
 
 export default function RootLayout({
